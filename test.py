bb4d6bcb9c585ad772ec9fb021bd00ec93f1aee3
Jaewon <protectingjw@naver.com>
2023-12-24 02:09:52 +0900

    commit

--- test.py
+++ test.py
@@ -1,20 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By 
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome() 
+# 크롬 드라이버 자동 업데이트을 위한 모듈
+# from webdriver_manager.chrome import ChromeDriverManager
+
+# 브라우저 꺼짐 방지 옵션
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options) 
 # selenium webdriver 에서 크롬 브라우저 지원 중
-driver.get('http://comp.fnguide.com')
+driver.get('http://www.naver.com')
 
-driver.switch_to.window( driver.window_handles[1] )
-
-
-print(driver.window_handles)
+# driver.implicitly_wait(3)
 
 
-while(True):
-    pass
+# driver.switch_to.window( driver.window_handles[1] )
 
-##
+
+# print(driver.window_handles)
+
 
 """""
 selector = ""
